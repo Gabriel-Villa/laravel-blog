7f4530e3185990639201c674b92628d9fccee815
Gabriel <VillaCapurroGabriel@gmail.com>
2020-09-05 20:44:31 -0500

    Controllers & Basic Routes

--- resources/views/admin/layouts/aside.blade.php
+++ resources/views/admin/layouts/aside.blade.php
@@ -1,8 +1,8 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <a href="{{ route('admin.home') }}" class="brand-link">
+        <img src="{{ asset('admin/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">Villa Sac Blog</span>
     </a>
@@ -12,10 +12,10 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('admin/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Gabriel Villa</a>
+                <a href="{{ route('admin.home') }}" class="d-block">Gabriel Villa</a>
             </div>
         </div>
 
@@ -23,8 +23,8 @@
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item has-treeview menu-open">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-home"></i>
                         <p>
                             Blog
                             <i class="right fas fa-angle-left"></i>
@@ -32,25 +32,25 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="./index.html" class="nav-link">
+                            <a href="{{ route('post.index') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Posts</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="./index2.html" class="nav-link">
+                            <a href="{{ route('category.index') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Category</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="./index3.html" class="nav-link">
+                            <a href="{{ route('tag.index') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Tags</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="./index3.html" class="nav-link">
+                            <a href="{{ route('user.index') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Users</p>
                             </a>
--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,28 @@ Route::get('post', function () {
     return view('user/post');
 })->name('post');
 
-
+/*
 Route::get('admin/home', function () {
     return view('admin/home');
 })->name('admin');
+*/
+
+//User routes
+Route::group(['namespace' => 'User'], function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('post', 'PostController@index')->name('post');
+});
 
 
-Route::get('admin/post', function () {
-    return view('admin/post/post');
-})->name('admin.post');
+//Admin routes
 
+Route::group(['namespace' => 'Admin'], function () {
 
-Route::get('admin/tag', function () {
-    return view('admin/tag/tag');
-})->name('admin.tag');
+    Route::get('admin/home', 'HomeController@index')->name('admin.home');
+    Route::resource('admin/post', 'PostController');
+    Route::resource('admin/category', 'CategoryController');
+    Route::resource('admin/tag', 'TagController');
+    Route::resource('admin/user', 'UserController');
 
-Route::get('admin/category', function () {
-    return view('admin/category/category');
-})->name('admin.category');
+});
+
